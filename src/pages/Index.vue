7803507180a94f46daf0983ea8f89a6f60b2c69b
Divyanshu Saini <bat2268@gmail.com>
2021-03-29 17:26:19 +0530

    created  web version of wether app

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,27 +1,55 @@
 <template>
-  <q-page class="flex column">
+  <q-page class="flex column" :class="bgClass">
     <div class="col q-pt-lg q-px-md">
-      <q-input v-model="search" placeholder="Search" dark borderless>
+      <q-input
+        v-model="search"
+        placeholder="Search"
+        dark
+        borderless
+        @keyup.enter="getWetherBySearch"
+      >
         <template v-slot:before>
-          <q-icon name="my_location" />
+          <q-icon name="my_location" @click="getLocation" />
         </template>
 
         <template v-slot:append>
-          <q-btn round dense flat icon="search" />
+          <q-btn round dense flat icon="search" @click="getWetherBySearch" />
         </template>
       </q-input>
     </div>
-    <div class="col text-white text-center">
-      <div class="text-h4 text-weight-light">Virar</div>
-      <div class="text-h6 text-weight-light">Rain</div>
-      <div class="text-h1 text-weight-thin q-my-lg relative-position">
-        <span>40</span>
-        <span class="text-h4 relative-position degree">&deg;</span>
+
+    <template v-if="wetherData">
+      <div class="col text-white text-center">
+        <div class="text-h4 text-weight-light">{{ wetherData.name }}</div>
+        <div class="text-h6 text-weight-light">
+          {{ wetherData.weather[0].main }}
+        </div>
+        <div class="text-h1 text-weight-thin q-my-lg relative-position">
+          <span>{{ Math.round(wetherData.main.temp) }}</span>
+          <span class="text-h4 relative-position degree">&deg;C</span>
+        </div>
       </div>
-    </div>
-    <div class="col text-center">
-      <img src="https://www.fillmurray.com/100/100" alt="Bill" />
-    </div>
+
+      <div class="col text-center">
+        <img
+          :src="`http://openweathermap.org/img/wn/${wetherData.weather[0].icon}@2x.png`"
+        />
+      </div>
+    </template>
+
+    <template v-else>
+      <div class="col column text-center tex-whhite">
+        <div class="col text-h2 text-weight-thin">
+          Quasar <br />
+          Wether
+        </div>
+        <q-btn class="col" flat @click="getLocation">
+          <q-icon left size="3em" name="my_location" />
+          <div>Find My Location</div>
+        </q-btn>
+      </div>
+    </template>
+
     <div class="cole skyline"></div>
   </q-page>
 </template>
@@ -32,27 +60,67 @@ export default {
   data() {
     return {
       search: "",
+      wetherData: null,
+      lat: null,
+      long: null,
+      apiUrl: "https://api.openweathermap.org/data/2.5/weather",
+      apiKey: "b038c215e2a6373388935bfa6d7c3a76",
     };
+  },
+  computed: {
+    bgClass() {
+      if (this.wetherData) {
+        if (this.wetherData.weather[0].icon.endsWith("n")) {
+          return "bg-night";
+        } else {
+          return "bg-day";
+        }
+      }
+    },
+  },
+  methods: {
+    getLocation() {
+      this.$q.loading.show();
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log("postion :", position);
+        this.lat = position.coords.latitude;
+        this.long = position.coords.longitude;
+        this.getWetherByCoords();
+      });
+    },
+    getWetherByCoords() {
+      this.$q.loading.show();
+      this.$axios(
+        `${this.apiUrl}?lat=${this.lat}&lon=${this.long}&appid=${this.apiKey}&units=Metric`
+      ).then((response) => {
+        console.log("response :", response);
+        this.wetherData = response.data;
+        this.$q.loading.hide();
+      });
+    },
+    getWetherBySearch() {
+      this.$q.loading.show();
+      this.$axios(
+        `${this.apiUrl}?q=${this.search}&appid=${this.apiKey}&units=Metric`
+      ).then((response) => {
+        console.log("response :", response);
+        this.wetherData = response.data;
+        this.$q.loading.hide();
+      });
+    },
   },
 };
 </script>
 
 <style lang="scss">
 .q-page {
-  background: #00416a; /* fallback for old browsers */
-  background: -webkit-linear-gradient(
-    to left,
-    #ffe000,
-    #799f0c,
-    #00416a
-  ); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(
-    to left,
-    #ffe000,
-    #799f0c,
-    #00416a
-  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */ /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: linear-gradient(to bottom, #136a8a, #267871);
+  &.bg-day {
+    background: linear-gradient(to bottom, #00b4db, #0083b0);
+  }
+  &.bg-night {
+    background: linear-gradient(to bottom, #232526, #414385);
+  }
 }
 .degree {
   top: -44px;
